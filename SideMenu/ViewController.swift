0941b49b2e5c3f97389629581e7cdee5f73f78dd
okue_hidetaka <okue@as-systems.jp>
2024-05-17 22:39:28 +0900

    firstCommit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by 奥江英隆 on 2024/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
